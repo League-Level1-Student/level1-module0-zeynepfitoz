7257e5ef40075c6cd4f60afdf201e58260803df2
1luckybiscuit <lukasnepo11@gmail.com>
2018-11-17 11:29:49 -0800

    dueling buttons

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -28,32 +28,44 @@ public class DuelingButtons implements ActionListener {
 
 	private void createUI() {
 		// 1. Add the panel to the frame
-
+frame.add(panel);
 		// 2. Make the frame visible
-
+frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+leftButton.setText("Click Me");
 		// 4. Set the text of the rightButton to "Click me!"
-
+rightButton.setText("Click Me");
 		// 5. Add an action listener to the leftButton
-
+leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+leftButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+panel.add(leftButton);
 		// 8. Add the rightButton to the panel
+panel.add(rightButton);
 
 		// 9. Pack the frame
-
+frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+frame.setTitle("Demanding Buttons ");
+	
+	
 	}
-
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		JButton buttonPressed = (JButton) arg0.getSource();
-		
-		
+		if(leftButton==buttonPressed) {
+			rightButton.setText("No, CLICK ME");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setPreferredSize(SMALL);
+		}
+		else if(rightButton==buttonPressed) {
+			leftButton.setText("No, CLICK ME");
+		leftButton.setPreferredSize(BIG);
+	rightButton.setText("Click me");
+			rightButton.setPreferredSize(SMALL);
+		}
+		 
 		/* If the buttonPressed was the leftButton....*/
 			// Set the text of the rightButton to "No, click Me!"
 			// Set the PREFERRED size of the rightButton to BIG
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,9 +22,9 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-String  photo = https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjc5v-bqdXeAhXCSt8KHTNkDQwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.chipotle.com%2Fdelivery&psig=AOvVaw1ui9mpD0hefYTBZyapKQR7&ust=1542334315226514
+String photo = "https://static1.squarespace.com/static/54e8ba93e4b07c3f655b452e/t/56c2a04520c64707756f4267/1493764650017/";
 		// 2. create a variable of type "Component" that will hold your image
-
+	
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
