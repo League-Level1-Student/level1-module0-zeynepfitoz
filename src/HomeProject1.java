3f8b2c3bbbd6ac88e0cca974ddcc1307e73d636d
zeynepfitoz <zeynepfitoz@192.168.0.19>
2018-11-19 08:07:25 -0800

    home project1

---
+++ src/HomeProject1.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class HomeProject1 {
+public static void main(String[] args) {
+	JLabel robotLabelWithImage;
+	JFrame m = new JFrame();
+	JPanel z = new JPanel();
+	m.setVisible(true);
+	m.setSize(700, 700);
+	m.setTitle("Robot");
+	m.add(z);
+	String robot = "robot.jpeg";
+	
+}
+}
